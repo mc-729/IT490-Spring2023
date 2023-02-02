14613bb8956c5868ff9646e3fb88f7ad1090032d
Paul <plim2282@gmail.com>
2023-02-02 00:47:28 -0500

    Created php files that create the db and users table

---
+++ database/createdb.php
@@ -0,0 +1,23 @@
+#!/usr/bin/php
+<?php
+$servername = "localhost";
+$username = "testuser";
+$password = "12345";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+$sql = "CREATE DATABASE IT490";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
---
+++ database/createuserstable.php
@@ -0,0 +1,33 @@
+#!/usr/bin/php
+<?php
+$servername = "localhost";
+$username = "testuser";
+$password = "12345";
+$dbname = "IT490";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to create table
+$sql = "CREATE TABLE IF NOT EXISTS `Users` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(60) NOT NULL,
+    `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE (`email`)
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table Users created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>

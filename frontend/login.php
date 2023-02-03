0cb113b987fd4aa35709701541623c88d0cf98ac
jgwentworth92 <alphabetboi92@protonmail.com>
2023-02-03 12:44:37 -0500

    we can echo stuff

--- frontend/login.php
+++ frontend/login.php
@@ -1,25 +1,69 @@
 <?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
 
 
-if (!isset($_POST))
+
+
+/*
+
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+if (isset($argv[1]))
 {
-	$msg = "NO POST MESSAGE SET";
-	echo json_encode($msg);
-	exit(0);
+  $msg = $argv[1];
 }
-$request = $_POST;
-$data = array();
-echo $response;
-switch ($request["submit"])
+else
 {
-	case "login&uname":
-		$data = array('email' => $_POST['email'],
-					  'password' => $_POST['pw'],				  
-	);
-	echo "Your login data is ".$data. "<br>";
-	break;
+  $msg = "test message";
 }
-echo json_encode($response);
-exit(0);
+*/
+if (isset ($_GET["email"]))
+{
+        $uname = $_GET["email"];
+}
+else{
 
+  $uname ="test";}
+  if (isset ($_GET["pw"]))
+{
+        $password = $_GET["pw"];
+}
+else{
+
+  $password ="test_password";}
+ /* 
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+$request = array();
+$request['type'] = "Login";
+$request['username'] = $uname;
+$request['password'] = $password;
+$request['message'] = $msg;
+$response = $client->send_request($request);
+//$response = $client->publish($request);
+
+echo "client received response: ".PHP_EOL;
+print_r($response);
+echo "\n\n";
+
+echo $argv[0]." END".PHP_EOL;
+*/
 ?>
+
+
+
+
+
+</script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+<div class="container-fluid">
+<?php 
+   
+   echo "<div> POST BODY <br>".$uname."</div>";  
+    echo "<div> POST BODY <br>".$password."</div>";      
+    
+?>
+we in hurrr????
+  <div>

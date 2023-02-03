91090dc95393633e0dc31ba8bc93449c8a93f30e
mc729 <mc729@IT-490.myguest.virtualbox.org>
2023-02-03 00:26:48 -0500

    Modified createuserstable.php, should now automatically create Users table within IT490 Database ONLY, cleaned up presentation of results in terminal with PHP_EOL

--- database/createuserstable.php
+++ database/createuserstable.php
@@ -10,23 +10,28 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
+} else 
+{
+	echo "Successfully Connected!".PHP_EOL;
 }
 
-// sql to create table
-$sql = "CREATE TABLE IF NOT EXISTS `Users` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `email` VARCHAR(100) NOT NULL,
-    `password` VARCHAR(60) NOT NULL,
-    `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`),
-    UNIQUE (`email`)
+// sql to create table Users
+$sql = "CREATE TABLE IT490.Users(
+	User_ID INT NOT NULL AUTO_INCREMENT,
+	F_Name VARCHAR(30) NOT NULL,
+	L_Name VARCHAR(30) NOT NULL,
+	Email VARCHAR(255) NOT NULL,
+	Password VARCHAR(80) NOT NULL,
+	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	modified TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (User_ID),
+	UNIQUE (Email)
 )";
 
 if ($conn->query($sql) === TRUE) {
-  echo "Table Users created successfully";
+  echo "Table Users created successfully".PHP_EOL;
 } else {
-  echo "Error creating table: " . $conn->error;
+  echo "Error creating table Users: " . $conn->error;
 }
 
 $conn->close();

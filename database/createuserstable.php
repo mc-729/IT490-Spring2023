c0f2b66acb404246628bfac14a9cdde64ffc09fb
Sungyoon Paul Lim <plim2282@gmail.com>
2023-02-02 21:03:32 -0500

    Modified createuserstable to show successful connection

--- database/createuserstable.php
+++ database/createuserstable.php
@@ -10,18 +10,21 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
+} else {
+  echo "Connection Successful".PHP_EOL;	
 }
 
 // sql to create table
-$sql = "CREATE TABLE IF NOT EXISTS `Users` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `email` VARCHAR(100) NOT NULL,
-    `password` VARCHAR(60) NOT NULL,
-    `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`),
-    UNIQUE (`email`)
-)";
+$sql = "Use IT490;";
+    "CREATE TABLE IF NOT EXISTS `Users` (".
+    "`id` INT NOT NULL AUTO_INCREMENT,".
+    "`email` VARCHAR(100) NOT NULL,".
+    "`password` VARCHAR(60) NOT NULL,".
+    "`created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,".
+    "`modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,".
+    " PRIMARY KEY (`id`),".
+    " UNIQUE (`email`)".
+");";
 
 if ($conn->query($sql) === TRUE) {
   echo "Table Users created successfully";

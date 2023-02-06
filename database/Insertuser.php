109558e851839cbbba84a947cff9d69d86f6598e
Paul <plim2282@gmail.com>
2023-02-06 17:36:46 -0500

    Created script to add a user

---
+++ database/Insertuser.php
@@ -0,0 +1,30 @@
+#!/usr/bin/php
+<?php
+$servername = "localhost";
+$username = "testuser";
+$password = "12345";
+$dbname = "IT490";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} else 
+{
+	echo "Successfully Connected!".PHP_EOL;
+}
+
+// sql to create table Users
+$sql = "INSERT INTO IT490.Users (User_ID, F_Name, L_Name, Email, Password)
+VALUES (1, Bob, Smith, bsmith@gmail.com, password);";
+
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table Users created successfully".PHP_EOL;
+} else {
+  echo "Error creating table Users: " . $conn->error;
+}
+
+$conn->close();
+?>

978240f53e559717a15f952d6cf5812aac6b1dc2
Sungyoon Paul Lim <plim2282@gmail.com>
2023-02-07 12:59:49 -0500

    Put the db connection into dologin function and fixed the variable names. Also changed the query to only check username for now.

--- authentication/AuthServer.php
+++ authentication/AuthServer.php
@@ -5,22 +5,36 @@ require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
 function doLogin($username,$password)
-{	
-    // lookup username in database
-	$sql = "SELECT * FROM IT490.Users WHERE username = '$username' AND password = '$password'";
+{
+	$servername = "localhost";
+	$uname = "testuser";
+	$pw = "12345";
+	$dbname = "IT490";
+
+// Create connection
+	$conn = new mysqli($servername, $uname, $pw, $dbname);
+// Check connection
+	if ($conn->connect_error) {
+  		die("Connection failed: " . $conn->connect_error);
+	} else {
+        echo "Successfully Connected!".PHP_EOL;
+	}
+	
+// lookup username in database
+	$sql = "SELECT * FROM IT490.Users WHERE Email = '$username'";
 	$result = mysqli_query($conn, $sql);
-	$row = mysqli_fetch_array($reult, MYSQLI_ASSOC);
+	$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 	$count = mysqli_num_rows($result);
 
 	if($count != 0)
 	{
 		echo "Login Succesful".PHP_EOL;
-		return true;
+		return "true";
 	}
 	else
 	{
 		echo "Login Failed".PHP_EOL;
-		return false;
+		return "false";
 	}
 }//End function doLogin
 
@@ -36,30 +50,15 @@ function requestProcessor($request)
   }
   switch ($request['type'])
   {
-    case "login":
+    case "Login":
       return doLogin($request['username'],$request['password']);
     case "validate_session":
       return doValidate($request['sessionId']);
   }
-  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+  return array("returnCode" => '0', 'message'=>"Server received request and processed!!!!!!!!!");
 }
 
 $server = new rabbitMQServer("RabbitMQConfig.ini","testServer");
-
-$servername = "localhost";
-$username = "testuser";
-$password = "12345";
-$dbname = "IT490";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-} else 
-{
-        echo "Successfully Connected!".PHP_EOL;
-}
 
 echo "Authentication Server BEGIN".PHP_EOL;
 $server->process_requests('requestProcessor');

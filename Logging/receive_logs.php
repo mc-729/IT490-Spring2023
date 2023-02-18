77b6049530c98c07bbf316e0d7c3738b5f1ac17a
Justin Gomez <justingomez705@gmail.com>
2023-02-18 15:15:55 -0500

    Added logging logic to receive_logs.php

--- Logging/receive_logs.php
+++ Logging/receive_logs.php
@@ -10,21 +10,28 @@ $channel->exchange_declare('eventFanout1', 'fanout', false, false, false);
 
 list($queue_name, ,) = $channel->queue_declare("", false, false, true, false);
 $channel->queue_bind($queue_name,'eventFanout1');
-/*
-$severities = array_slice($argv, 1);
-if (empty($severities)) {
-    file_put_contents('php://stderr', "Usage: $argv[0] [info] [warning] [error]\n");
-    exit(1);
+
+echo "Starting Logs Server";
+function doLog($msg,$type,$origin)
+{
+  $date = new DateTime('now');
+  $date = $date->format("m/d/y h:i:s");
+  echo "[$date] $origin [Type of msg: $type] [LOG: $msg]";
+    
 }
 
-foreach ($severities as $severity) {
-    $channel->queue_bind($queue_name, 'direct_logs', $severity);
-}*/
-
-echo " [*] Waiting for logs. To exit press CTRL+C\n";
-
 $callback = function ($msg) {
-    echo ' [x] ', $msg->delivery_info['routing_key'], ':', $msg->body, "\n";
+    
+    switch ($msg['service']){
+        case "database":
+            $originator = 'Database Server:';
+            return doLog($msg['message'], $msg['type'], $originator);
+
+        case "database":
+            $originator = 'Database Server:';
+            return doLog($msg['message'], $msg['type'], $originator);
+        
+        }
 };
 
 $channel->basic_consume($queue_name, '', false, true, false, false, $callback);

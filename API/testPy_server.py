fc44eee8e07c843a8f45387e1048dcf8b6676e4d
Justin Gomez <justingomez705@gmail.com>
2023-02-21 22:15:48 -0500

    changed client and server for testing rabbit

--- API/testPy_server.py
+++ API/testPy_server.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import pika
-import cocktail_api.py
+import requests
+import json
+import os
+import api_keys
+from API import cocktail_api
 
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1', '5672', 'testHost', pika.PlainCredentials('test', 'test')))
@@ -8,7 +12,25 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1', '567
 channel = connection.channel()
 
 channel.queue_declare(queue='rpc_queue')
+headers = {
+	"X-RapidAPI-Key": api_keys.api_key, # please keep the 100/day limit in mind for the Cocktail DB, if you are working on it 
+    "X-RapidAPI-Host": "the-cocktail-db.p.rapidapi.com",                    # just input your api key so you can keep track of calls
+	"Content-Type": "application/json"                                                                      
+}
+def search_by_name(dictionary):
+    # returns all cocktails with Ingredient in the name ex Vodka -> Vodka fizz, Vodka Martini
+    url = "https://the-cocktail-db.p.rapidapi.com/search.php"
+    
+    querystring = {dictionary['operation']:dictionary['ingredient']}
+    print(querystring)
+    response = requests.request("GET", url, headers=headers, params=querystring)
 
+    #make a call to json to file function to cache data
+
+
+    # turns response to json and prints it nicely
+    response = response.json()
+    #print(json.dumps(response, indent=2))
 def api_call(body):
      return search_by_name(body)
 
@@ -19,7 +41,7 @@ def on_request(ch, method, props, body):
     n = body
 
     print("we recieved" % n)
-    response = api_cal(n)
+    response = api_call(n)
 
     ch.basic_publish(exchange='',
                      routing_key=props.reply_to,

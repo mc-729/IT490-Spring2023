4cd98a528aadbdb965f976aa012681aa4b840dea
Justin Gomez <justingomez705@gmail.com>
2023-02-21 20:28:35 -0500

    Adding rabbitMQ server functionality

---
+++ API/app.py
@@ -0,0 +1,19 @@
+import asyncio
+import json
+import math
+import pika
+
+async def main():
+
+    print("Opening connection to RabbitMQ...")
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1', '5672', 'testHost', pika.PlainCredentials('test', 'test')))
+    except:
+        print("Failed to connect to RabbitMQ. Check that the connection parameters are correct.")
+        return
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='testExchange', exchange_type='direct')
+    print("Declaring queue variables...")
+    channel.queue_declare(queue="testQueue", durable=True, arguments={"x-queue-type:": "quorum"})
+    channel.queue_bind(exchange='testExchange', queue="testQueue")

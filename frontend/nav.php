3c06ac46866def25d1a5daca1162681bd679736a
jgwentworth92 <alphabetboi92@protonmail.com>
2023-02-26 17:18:21 -0500

    added debug search page and fixed single result not displaying,i was icrementing var in wrong place

--- frontend/nav.php
+++ frontend/nav.php
@@ -25,8 +25,10 @@
             <?php endif; ?>        
             <li class="nav-item"><a class="nav-link" href="/RegisterForm.php">Register</a></li>
             <li class="nav-item"><a class="nav-link" href="/validate_test.php">session valid?</a></li> 
-            <li class="nav-item"><a class="nav-link" href="/apt_search.php">search</a></li>                
+            <li class="nav-item"><a class="nav-link" href="/apt_search.php">search</a></li>      
+            <li class="nav-item"><a class="nav-link" href="/debugSearch.php"> debug search</a></li>            
             </ul>
         </div>
     </div>
 </nav>
+ssss

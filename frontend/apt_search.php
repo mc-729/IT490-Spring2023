b1f4362bd26cf491f08b34dbc5aeaff1e48d6b06
jgwentworth92 <alphabetboi92@protonmail.com>
2023-02-27 14:36:36 -0500

    merge conflict fixs

--- frontend/apt_search.php
+++ frontend/apt_search.php
@@ -83,6 +83,7 @@ $count = 0
                         <?php 
                         endfor; ?>
                     </ul>
+                    
                     <div class="card-body">
                         <a href="#" class="card-link">Card link</a>
                         <a href="#" class="card-link">Another link</a>

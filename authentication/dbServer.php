838322c7938a2e9b19ddf1b77486274eceb38c9a
jgwentworth92 <alphabetboi92@protonmail.com>
2023-03-16 12:34:00 -0400

    big unicode vibe

--- authentication/dbServer.php
+++ authentication/dbServer.php
@@ -32,7 +32,7 @@ function loginAuth($username, $password)
 
         if (password_verify($password, $hashedpass)) {
             echo 'Login Successful' . PHP_EOL;
-			$resp =array(
+            $resp = array(
                 'login_status' => true,
                 'session_id' => SessionGen($row['User_ID']),
                 'user_id' => $row['User_ID'],
@@ -44,7 +44,7 @@ function loginAuth($username, $password)
             return $resp;
         } else {
             echo 'Login Failed' . PHP_EOL;
-            $resp =array(
+            $resp = array(
                 'login_status' => false,
                 'session_id' => null,
                 'user_id' => null,
@@ -57,7 +57,7 @@ function loginAuth($username, $password)
         }
     } else {
         echo 'Login Failed' . PHP_EOL;
-        $resp =array(
+        $resp = array(
             'login_status' => false,
             'session_id' => null,
             'user_id' => null,
@@ -68,7 +68,6 @@ function loginAuth($username, $password)
         );
         return $resp;
     }
-
 } //End loginAuth
 
 function dbConnection()
@@ -114,8 +113,8 @@ function registrationInsert($username, $password, $email, $firstName, $lastName)
         // ==1 means found an already existing Username/Email in IT490.Users
         echo 'Username/Email already exists, please use a different one.' .
             PHP_EOL;
-            $resp = ['login_status' => false];
-            return $resp;
+        $resp = ['login_status' => false];
+        return $resp;
     }
     //If Username/Email is not found in database/doesn't exist, do this
     else {
@@ -128,7 +127,7 @@ function registrationInsert($username, $password, $email, $firstName, $lastName)
             $resp = ['login_status' => true];
             return $resp;
         } else {
-           /* $msg = 'Error with query';
+            /* $msg = 'Error with query';
             $request = [];
             $request['type'] = 'error';
             $request['service'] = 'database';
@@ -211,214 +210,278 @@ function eventInsert($name, $UID, $description, $date, $url, $image)
         $request['type'] = 'error';
         $request['service'] = 'database';
         $request['message'] = $msg;
-        sendLog($request); 
+        sendLog($request);
         echo "we failed to insert bbby";
     }
-    
 } // End eventInsert
 
 
 
-function updateProfile($sessionid, $username,$newpassword, $oldpassword, $email, $firstName, $lastName) {
+function updateProfile($sessionid, $username, $newpassword, $oldpassword, $email, $firstName, $lastName)
+{
     // Connect to the database
     $conn = dbConnection();
-    
+
     // Build the SQL statement
     $sql = "UPDATE Users SET";
-   
-    if(doValidate($sessionid)) {
-		$sql2 = "SELECT UID FROM IT490.sessions WHERE sessionID = '$sessionid'";
-		$result = mysqli_query($conn, $sql2);
-		$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-		$userid = $row['UID'];
 
-   
-    if (!empty($newpassword) && !empty($oldpassword)) {
-		$sql2 = "SELECT Password FROM IT490.Users WHERE User_ID = '$userid'";
-		$result2 = mysqli_query($conn, $sql2);
-		$row2 = mysqli_fetch_array($result2, MYSQLI_ASSOC);
-		$hashedpass = $row2['Password'];
-		if (password_verify($oldpassword, $hashedpass)) {
-			$hashPassword = password_hash($newpassword, PASSWORD_DEFAULT);
-			$sql .= " Password='".mysqli_real_escape_string($conn, $hashPassword)."',"; } 
-    }
-  
-	if (!empty($username)) {
-        $sql .= " Username='".mysqli_real_escape_string($conn, $username)."',";
-    }
-   
-    if (!empty($email)) {
-        $sql .= " Email='".mysqli_real_escape_string($conn, $email)."',";
-    }
-    if (!empty($firstName)) {
-        $sql .= " f_name='".mysqli_real_escape_string($conn, $firstName)."',";
-    }
-    if (!empty($lastName)) {
-        $sql .= " l_name='".mysqli_real_escape_string($conn, $lastName)."',";
-    }
-    
-    // Remove the trailing comma from the SQL statement
-    $sql = rtrim($sql, ",");
-    
-    // Add the WHERE clause to the SQL statement
-    $sql .= " WHERE User_ID=".$userid;
-    
-    // Execute the SQL statement
-    $result = mysqli_query($conn, $sql);
-	if($result){return true;}
+    if (doValidate($sessionid)) {
+        $sql2 = "SELECT UID FROM IT490.sessions WHERE sessionID = '$sessionid'";
+        $result = mysqli_query($conn, $sql2);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $userid = $row['UID'];
 
 
+        if (!empty($newpassword) && !empty($oldpassword)) {
+            $sql2 = "SELECT Password FROM IT490.Users WHERE User_ID = '$userid'";
+            $result2 = mysqli_query($conn, $sql2);
+            $row2 = mysqli_fetch_array($result2, MYSQLI_ASSOC);
+            $hashedpass = $row2['Password'];
+            if (password_verify($oldpassword, $hashedpass)) {
+                $hashPassword = password_hash($newpassword, PASSWORD_DEFAULT);
+                $sql .= " Password='" . mysqli_real_escape_string($conn, $hashPassword) . "',";
+            }
+        }
+
+        if (!empty($username)) {
+            $sql .= " Username='" . mysqli_real_escape_string($conn, $username) . "',";
+        }
+
+        if (!empty($email)) {
+            $sql .= " Email='" . mysqli_real_escape_string($conn, $email) . "',";
+        }
+        if (!empty($firstName)) {
+            $sql .= " f_name='" . mysqli_real_escape_string($conn, $firstName) . "',";
+        }
+        if (!empty($lastName)) {
+            $sql .= " l_name='" . mysqli_real_escape_string($conn, $lastName) . "',";
+        }
+
+        // Remove the trailing comma from the SQL statement
+        $sql = rtrim($sql, ",");
+
+        // Add the WHERE clause to the SQL statement
+        $sql .= " WHERE User_ID=" . $userid;
+
+        // Execute the SQL statement
+        $result = mysqli_query($conn, $sql);
+        if ($result) {
+            return true;
+        }
+    } else {
+        logout($sessionid);
+    }
 }
 
-	else {logout($sessionid);}
-
-
-}
-
-function storeSearchResultsInCache($query,$searchResults)
-{	
+function storeSearchResultsInCache($query, $searchResults)
+{
     $obj = json_decode($searchResults, true);
     echo gettype($obj);
-    $count=0;
-  
-    
-	
-	// Convert results to JSON
-	$json = json_encode($searchResults);
-	$filtered_json = "[".filter_var($json)."]";
-    $query=implode(',',$query);
-	print_r($json);
+    $count = 0;
 
-    
 
-	// Insert JSON data into database using prepared statement
 
-	$conn = dbConnection();
-	$stmt = $conn->prepare('INSERT INTO IT490.Cache (SearchKey, Results) VALUES (?,?)');
-	$stmt->bind_param('ss', $query, $filtered_json);
-	$result = $stmt->execute();
-	echo $result;
-	$stmt->close();
-	$conn->close();
+    // Convert results to JSON
+    $json = json_encode($searchResults);
+    $filtered_json = "[" . filter_var($json) . "]";
+    $query = implode(',', $query);
+    print_r($json);
 
-	// Check for errors and return result
-	if ($result) {
-		echo "It has been added to the cache ". PHP_EOL;
-      
-		return true;
-	} else {
-		echo "Something went wrong in the cache". PHP_EOL;
-		return false;
-	}
-	
-	
+
+
+    // Insert JSON data into database using prepared statement
+
+    $conn = dbConnection();
+    $stmt = $conn->prepare('INSERT INTO IT490.Cache (SearchKey, Results) VALUES (?,?)');
+    $stmt->bind_param('ss', $query, $filtered_json);
+    $result = $stmt->execute();
+    echo $result;
+    $stmt->close();
+    $conn->close();
+
+    // Check for errors and return result
+    if ($result) {
+        echo "It has been added to the cache " . PHP_EOL;
+
+        return true;
+    } else {
+        echo "Something went wrong in the cache" . PHP_EOL;
+        return false;
+    }
 }
 
-function requestEmail($userid){
-	$conn = dbConnection();
+function requestEmail($userid)
+{
+    $conn = dbConnection();
     $query = "SELECT Email FROM IT490.Users WHERE User_Id = '$userid'";
-	$result = mysqli_query($conn, $query);
-	$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-	$email = $row['Email'];
-	echo $email . PHP_EOL;
-	mysqli_close($conn);
-	return $email;
-
+    $result = mysqli_query($conn, $query);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $email = $row['Email'];
+    echo $email . PHP_EOL;
+    mysqli_close($conn);
+    return $email;
 } // End requestEmail
 
-function requestEvents($timeleft){
-	$conn = dbConnection();
+function requestEvents($timeleft)
+{
+    $conn = dbConnection();
     $query = "SELECT * FROM IT490.events WHERE timeleft <= '$timeleft'";
-	$result = mysqli_query($conn, $query);
-	$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-	//mysqli_free_result($rows);
-	//echo $rows . PHP_EOL;
-	mysqli_close($conn);
-	return $rows;
-
+    $result = mysqli_query($conn, $query);
+    $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    //mysqli_free_result($rows);
+    //echo $rows . PHP_EOL;
+    mysqli_close($conn);
+    return $rows;
 } // End requestEvents
 
 function fetchSearchResultsCached($query)
 
 
-{  
-    try{
-     echo"did we make it here?". PHP_EOL;
-    
-     $strQuery=implode(',',$query);
-	$conn=dbConnection();
-	$sql="SELECT * FROM IT490.Cache WHERE SearchKey = '$strQuery'";
-	$result = mysqli_query($conn, $sql);
-	$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-	$count = mysqli_num_rows($result);
-	
+{
+    try {
+        echo "did we make it here?" . PHP_EOL;
 
-
-
-	if ($count == 0) {
-		echo "it was not in cache";
-        $client = new rabbitMQClient('RabbitMQConfig.ini', 'APIServer');
-
-        $searchResults = $client->send_request($query);
-        if(isset($searchResults)){
-		storeSearchResultsInCache($query,$searchResults);
-        $strQuery=implode(',',$query);
-        $conn=dbConnection();
-        $sql="SELECT * FROM IT490.Cache WHERE SearchKey = '$strQuery'";
+        $strQuery = implode(',', $query);
+        $conn = dbConnection();
+        $sql = "SELECT * FROM IT490.Cache WHERE SearchKey = '$strQuery'";
         $result = mysqli_query($conn, $sql);
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-      	return $row['Results'];}
-		
-	} else if($count!=0) {
-		echo "it was in cache";
-		$searchResults = $row['Results'];
-        echo gettype($searchResults);
-        return $searchResults;
-		//print_r($searchResults);
-	}}
-    catch (Exception $e) {
+        $count = mysqli_num_rows($result);
+
+
+
+
+        if ($count == 0) {
+            echo "it was not in cache";
+            $client = new rabbitMQClient('RabbitMQConfig.ini', 'APIServer');
+
+            $searchResults = $client->send_request($query);
+            if (isset($searchResults)) {
+                storeSearchResultsInCache($query, $searchResults);
+                $strQuery = implode(',', $query);
+                $conn = dbConnection();
+                $sql = "SELECT * FROM IT490.Cache WHERE SearchKey = '$strQuery'";
+                $result = mysqli_query($conn, $sql);
+                $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                return $row['Results'];
+            }
+        } else if ($count != 0) {
+            echo "it was in cache";
+            $searchResults = $row['Results'];
+            echo gettype($searchResults);
+            return $searchResults;
+            //print_r($searchResults);
+        }
+    } catch (Exception $e) {
         echo 'Caught exception my dude: ',  $e->getMessage(), "\n";
         return  $resp = ['API_REQUEST_STATUS' => false];
     }
-	
 }
 
 
-function myLiquorCabinetUpdateIngredients($sessionid, $user_ID, $recipeName, $ingredients){
+
+
+function retrieveRecipes($sessionid)
+
+{   $conn=dbconnection();
+     if (doValidate($sessionid)) {
+    
+    $sql = "SELECT UID FROM IT490.sessions WHERE sessionID = '$sessionid'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $userid = $row['UID'];
+    $sql2 = "SELECT Recipe FROM IT490.Cocktails WHERE User_ID = $userid";
+    $result2 = $conn->query($sql2);
+    $recipelist = mysqli_fetch_array($result2, MYSQLI_ASSOC);
+    $recipeData = json_decode($recipelist['Recipe'], true);
+
+    return $recipeData;
+}
+}
+
+function updateRecipeList($sessionid, $recipedata) {
+    $conn = dbConnection();
+    if (doValidate($sessionid)) {
+        $sql = "SELECT UID FROM IT490.sessions WHERE sessionID = '$sessionid'";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $userid = $row['UID'];
+        $sql2 = "SELECT Recipe FROM IT490.Cocktails WHERE User_ID = $userid";
+        $result2 = $conn->query($sql2);
+        $count = mysqli_num_rows($result2);
+        $recipedata = str_replace("'", '"', $recipedata);
+        echo "Recipe after taking out double qoutes" .$recipedata. PHP_EOL;
+        $recipedata = str_replace("None", "null", $recipedata);
+        echo "Recipe after replacing none" .$recipedata. PHP_EOL;
+     
+        
+        if ($count != 0) {
+            $recipelist = mysqli_fetch_array($result2, MYSQLI_ASSOC);
+            $recipeData = json_decode($recipelist['Recipe'], true);
+            array_push($recipeData, $recipedata);
+            $recipeData=json_encode($recipeData, JSON_HEX_QUOT);
+            $sql3 = "UPDATE IT490.Cocktails SET Recipe = '$recipeData' WHERE User_ID = $userid";
+            $result = mysqli_query($conn, $sql3);
+        } else {
+            $recipelist = array($recipedata);
+      
+           // $recipelist =json_encode($recipelist,  JSON_UNESCAPED_UNICODE|JSON_FORCE_OBJECT). "\n";
+           $recipelist=json_encode($recipelist, JSON_HEX_QUOT);
+
+          
+            $recipelistJson = json_encode($recipelist);
+            echo "Recipe after second encoding" .$recipelistJson. PHP_EOL;
+            $sql3 = "INSERT INTO IT490.Cocktails (User_ID, Recipe) VALUES ($userid, '$recipelist')";
+            $result = mysqli_query($conn, $sql3);
+        }
+        
+        if ($result) {
+            echo "Recipe updated successfully" . PHP_EOL;
+        } else {
+            echo "An error occurred while updating the recipe" . PHP_EOL;
+        }
+    }
+}
+
+
+function myLiquorCabinetUpdateIngredients($sessionid, $user_ID, $recipeName, $ingredients)
+{
     // Connect to the database
     $conn = dbConnection();
-   
+
     //from function UpdateProfile
-    if(doValidate($sessionid)) {
-       $sql = "SELECT UID FROM IT490.sessions WHERE sessionID = '$sessionid'";
-       $result = mysqli_query($conn, $sql);
-       $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-       $userid = $row['UID'];
+    if (doValidate($sessionid)) {
+        $sql = "SELECT UID FROM IT490.sessions WHERE sessionID = '$sessionid'";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $userid = $row['UID'];
+
+
+        //SQL select statement to then update said ingredients
+        $sql2 = "SELECT Ingredients FROM IT490.Cocktails WHERE User_ID = $user_ID";
+        $result2 = $conn->query($sql2);
+        $row2 = $result2->fetch_assoc();
+        $currentIngredients = json_decode($row2['Ingredients'], true);
+
+        //Update Ingredients
+        foreach ($ingredients as $ingredientName => $updatedIngredients) {
+            if ($updatedIngredients > 0) { //Change the current Ingredient quantity to its updated one
+                $currentIngredients[$ingredientName] = $updatedIngredients;
+            } else {
+                unset($currentIngredients[$ingredientName]); //if 0 then unset to remove
+            }
+        }
+
+        //Update cocktails table with updated Ingredeints
+        $updatedIngredients = json_encode($currentIngredients);
+        $sql3 = "UPDATE IT490.Cocktails SET Ingredients = '$updatedIngredients' WHERE User_ID = $user_ID";
+        if ($conn->query($sql3) === FALSE) {
+            echo "Error, could not update ingredients: " . $conn->error . PHP_EOL;
+        }
     }
-   
-    //SQL select statement to then update said ingredients
-    $sql2 = "SELECT Ingredients FROM IT490.Cocktails WHERE User_ID = $user_ID";
-    $result2 = $conn->query($sql2);
-    $row2 = $result2->fetch_assoc();
-    $currentIngredients = json_decode($row2['Ingredients'], true);
-   
-   //Update Ingredients
-   foreach ($ingredients as $ingredientName => $updatedIngredients){
-       if ($updatedIngredients > 0){//Change the current Ingredient quantity to its updated one
-           $currentIngredients[$ingredientName] = $updatedIngredients;
-       } else {
-           unset($currentIngredients[$ingredientName]);//if 0 then unset to remove
-       }
-   }
-   
-   //Update cocktails table with updated Ingredeints
-   $updatedIngredients = json_encode($currentIngredients);
-   $sql3 = "UPDATE IT490.Cocktails SET Ingredients = '$updatedIngredients' WHERE User_ID = $user_ID";
-   if ($conn->query($sql3) === FALSE){
-       echo "Error, could not update ingredients: " . $conn->error . PHP_EOL;
-   }
-   
-   /*
+
+
+    /*
    //Get information/data from the JSON file
    $info = json_decode(file_get_contents('ingredients.json'), true);
    
@@ -431,8 +494,7 @@ function myLiquorCabinetUpdateIngredients($sessionid, $user_ID, $recipeName, $in
    
    file_put_contents('ingredients.json', json_encode($info));
    */
-   
-   }
+}
 
 
 
@@ -444,8 +506,8 @@ function requestProcessor($request)
     if (!isset($request['type'])) {
         return 'ERROR: unsupported message type';
     }
-    switch ($request['type']) 
-       { case 'Login':
+    switch ($request['type']) {
+        case 'Login':
             return loginAuth($request['username'], $request['password']);
         case 'Register':
             return registrationInsert(
@@ -456,32 +518,31 @@ function requestProcessor($request)
                 $request['lastName']
             );
         case 'validate_session':
-		
+
             return doValidate($request['sessionID']);
         case 'Logout':
             return logout($request['sessionID']);
         case 'API_CALL':
-		    return  fetchSearchResultsCached($request['key']);
+            return  fetchSearchResultsCached($request['key']);
         case "Update":
-			return updateProfile($request['sessionID'],$request['username'],$request['newPW'],$request['oldPW'],$request['email'],$request['firstName'],$request['lastName']);
+            return updateProfile($request['sessionID'], $request['username'], $request['newPW'], $request['oldPW'], $request['email'], $request['firstName'], $request['lastName']);
         case 'Insertevent':
             return  eventInsert($request['name'], $request['UID'], $request['description'], $request['date'], $request['url'], $request['image']);
-			
-      case "Email":
-            return requestEmail($request['userid']);
-		case "Events":
-			return requestEvents($request['timeleft']);
-        case "like":
-                return storeSearchResultsInCache($request['drinkName'],$request['drink']);
-     
 
+        case "Email":
+            return requestEmail($request['userid']);
+        case "Events":
+            return requestEvents($request['timeleft']);
+        case "like":
+            return updateRecipeList($request['sessionID'], $request['drink'] );
+            case "retrieveRecipe":
+                return retrieveRecipes($request['sessionID']);
     }
     //$callLogin = array($callLogin => doLogin($username,$password)
     return [
         'returnCode' => '0',
         'message' => 'Server received the request and processed it.',
     ];
-
 } // End requestProcessor
 
 $server = new rabbitMQServer('RabbitMQConfig.ini', 'testServer');

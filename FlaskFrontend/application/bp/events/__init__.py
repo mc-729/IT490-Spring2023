5d505183d42188f73b3681b2c80d7e67c9037c13
Sungyoon Paul Lim <plim2282@gmail.com>
2023-03-18 19:55:57 -0400

    Completed events saving to the database

--- FlaskFrontend/application/bp/events/__init__.py
+++ FlaskFrontend/application/bp/events/__init__.py
@@ -2,46 +2,49 @@
 import ast
 import json
 from flask import Blueprint, jsonify, render_template, request, session
-from application.bp.authentication.forms import SearchForm , IngredientsForm, LikeButton, EventsForm
+from application.bp.authentication.forms import SearchForm, IngredientsForm, LikeButton, EventsForm
 from application.rabbitMQ.rabbitmqlibPYTHON import RabbitMQClient
 bp_events = Blueprint('events', __name__, template_folder='templates')
 
-    # Process the data and return a response    
-@bp_events.route('/sendEventData', methods=['GET','POST'])
-def sendEventData():
-     data = request.get_json()
-     print(data)
-     UID=session['user_id']
-    
-     data=json.dumps(data)
-     data=json.loads(data)
-     eventName=ast.literal_eval(data)["title"]
-     eventURL=ast.literal_eval(data)["link"]
-     eventImage=ast.literal_eval(data)["image"]
-     eventDescription=ast.literal_eval(data)["description"]
-     eventDate=ast.literal_eval(data)["date"]["start_date"]
-     print(eventDate)
-     client = RabbitMQClient('testServer')
-     request_dict = {
-                'type': 'SaveEvent',
-                'name': eventName,
-                'date': eventDate,
-                'image': eventImage,
-                'description': eventDescription,
-                'URL': eventURL, 
-                'UID': UID            
-            }
+# Process the data and return a response
 
-     response = client.send_request(request_dict)
-     if(response):
-        response = {"status": "success", "message": "Data received successfully."}
-     #   flash("you have liked the recipe for %s"%eventName, "success")
-     else:
+
+@bp_events.route('/sendEventData', methods=['GET', 'POST'])
+def sendEventData():
+    data = request.get_json()
+    UID = session["user_id"]
+
+    data = json.dumps(data)
+    data = json.loads(data)
+    eventName = ast.literal_eval(data)["title"]
+    eventURL = ast.literal_eval(data)["link"]
+    eventImage = ast.literal_eval(data)["image"]
+    eventDescription = ast.literal_eval(data)["description"]
+    eventDate = ast.literal_eval(data)["date"]["start_date"]
+
+    client = RabbitMQClient('testServer')
+    request_dict = {
+        'type': 'SaveEvent',
+        'name': eventName,
+        'date': eventDate,
+        'image': eventImage,
+        'description': eventDescription,
+        'URL': eventURL,
+        'UID': UID
+    }
+
+    response = client.send_request(request_dict)
+    if (response):
+        response = {"status": "success",
+                    "message": "Data received successfully."}
+    #   flash("you have liked the recipe for %s"%eventName, "success")
+    else:
         response = {"status": "error", "message": "something went wrong"}
-    
-     return jsonify(response)
+
+    return jsonify(response)
 
     # Return a response indicating the request was successful
+
 
 @bp_events.route('/events', methods=['GET', 'POST'])
 def events():
@@ -53,7 +56,7 @@ def events():
         search = request.form['search']
         city = str(request.form['city'])
         state = str(request.form['state'])
-        location = city +', '+ state
+        location = city + ', ' + state
 
         if search and location:
             client = RabbitMQClient('testServer')
@@ -64,20 +67,20 @@ def events():
                         'type': 'GoogleEventSearch',
                         'operation': 's',
                         'searchTerm': search,
-                        'location' : location
+                        'location': location
                     }
                 }
                 response = client.send_request(request_dict)
                 response = json.loads(json.loads(response))[0]
                 response = json.loads(response)
-                
+
                 for event in response:
                     start_dates.append(event['date']['start_date'])
                     print(start_dates)
 
-                #return jsonify(response)
+                # return jsonify(response)
                 data = response
-                
+
             except Exception as e:
                 print(str(e))
     else:
@@ -85,4 +88,3 @@ def events():
     if form.validate_on_submit():
         pass
     return render_template('events.html', form=form, data=data, start_dates=start_dates, like=like)
-
--- authentication/dbServer.php
+++ authentication/dbServer.php
@@ -193,7 +193,7 @@ function logout($sessionid)
 function eventInsert($name, $UID, $description, $date, $url, $image)
 {
     $conn = dbConnection();
-
+    echo "DB connected";
     $date_str = date_create_from_format('M d', $date);
     $date_str->setDate(date('Y'), $date_str->format('m'), $date_str->format('d'));
     $formatted_date = date_format($date_str, 'Y-m-d');
@@ -402,8 +402,7 @@ function requestProcessor($request)
                 $request['firstName'],
                 $request['lastName']
             );
-        case 'validate_session':
-		
+        case 'validate_session':		
             return doValidate($request['sessionID']);
         case 'Logout':
             return logout($request['sessionID']);
